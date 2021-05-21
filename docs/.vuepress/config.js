ca1508a4ffad25b5d4b6f6345dbb470ba1f86e3c
mgt <mgt@oi-wiki.org>
2021-05-21 08:37:24 +0000

    chore: add config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  base: '/HPC-roadmap/',
+  evergreen: true,
+  locales: {
+    '/': {
+      lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
+      title: '高性能计算学习路线',
+      description: '华中科技大学超算队',
+    },
+  }
+}
